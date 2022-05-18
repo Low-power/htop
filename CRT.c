3b3fef42061faea40eb92444d0fb0d8de2c163d8
WHR <msl0000023508@gmail.com>
2022-05-18 14:01:02 +0800

    Support loading user defined color schemes

--- CRT.c
+++ CRT.c
@@ -119,6 +119,7 @@ typedef enum {
 #include <execinfo.h>
 #endif
 #include <pwd.h>
+#include <dirent.h>
 
 #if defined ERR && ERR > 0
 #undef ERR
@@ -686,23 +687,236 @@ char *CRT_getConfigDirPath(const char **home_path_p) {
    return config_dir_path;
 }
 
+static const struct key_name {
+	const char *key_prefix;
+	unsigned int key_length;
+} color_element_name_map[] = {
+#define COLOR_ELEMENT_NAME(KEY) [HTOP_##KEY##_COLOR] = { #KEY "=", sizeof #KEY }
+	COLOR_ELEMENT_NAME(DEFAULT),
+	COLOR_ELEMENT_NAME(FUNCTION_BAR),
+	COLOR_ELEMENT_NAME(FUNCTION_KEY),
+	COLOR_ELEMENT_NAME(FAILED_SEARCH),
+	COLOR_ELEMENT_NAME(PANEL_HEADER_FOCUS),
+	COLOR_ELEMENT_NAME(PANEL_HEADER_UNFOCUS),
+	COLOR_ELEMENT_NAME(PANEL_SELECTION_FOCUS),
+	COLOR_ELEMENT_NAME(PANEL_SELECTION_FOLLOW),
+	COLOR_ELEMENT_NAME(PANEL_SELECTION_UNFOCUS),
+	COLOR_ELEMENT_NAME(LARGE_NUMBER),
+	COLOR_ELEMENT_NAME(METER_TEXT),
+	COLOR_ELEMENT_NAME(METER_VALUE),
+	COLOR_ELEMENT_NAME(LED_COLOR),
+	COLOR_ELEMENT_NAME(UPTIME),
+	COLOR_ELEMENT_NAME(BATTERY),
+	COLOR_ELEMENT_NAME(TASKS_RUNNING),
+	COLOR_ELEMENT_NAME(SWAP),
+	COLOR_ELEMENT_NAME(PROCESS),
+	COLOR_ELEMENT_NAME(PROCESS_SHADOW),
+	COLOR_ELEMENT_NAME(PROCESS_TAG),
+	COLOR_ELEMENT_NAME(PROCESS_MEGABYTES),
+	COLOR_ELEMENT_NAME(PROCESS_TREE),
+	COLOR_ELEMENT_NAME(PROCESS_R_STATE),
+	COLOR_ELEMENT_NAME(PROCESS_D_STATE),
+	COLOR_ELEMENT_NAME(PROCESS_BASENAME),
+	COLOR_ELEMENT_NAME(PROCESS_HIGH_PRIORITY),
+	COLOR_ELEMENT_NAME(PROCESS_LOW_PRIORITY),
+	COLOR_ELEMENT_NAME(PROCESS_KERNEL_PROCESS),
+	COLOR_ELEMENT_NAME(PROCESS_THREAD),
+	COLOR_ELEMENT_NAME(PROCESS_THREAD_BASENAME),
+	COLOR_ELEMENT_NAME(BAR_BORDER),
+	COLOR_ELEMENT_NAME(BAR_SHADOW),
+	COLOR_ELEMENT_NAME(GRAPH_1),
+	COLOR_ELEMENT_NAME(GRAPH_2),
+	COLOR_ELEMENT_NAME(MEMORY_USED),
+	COLOR_ELEMENT_NAME(MEMORY_BUFFERS),
+	COLOR_ELEMENT_NAME(MEMORY_BUFFERS_TEXT),
+	COLOR_ELEMENT_NAME(MEMORY_CACHE),
+	COLOR_ELEMENT_NAME(MEMORY_ZFS_ARC),
+	COLOR_ELEMENT_NAME(LOAD),
+	COLOR_ELEMENT_NAME(LOAD_AVERAGE_FIFTEEN),
+	COLOR_ELEMENT_NAME(LOAD_AVERAGE_FIVE),
+	COLOR_ELEMENT_NAME(LOAD_AVERAGE_ONE),
+	COLOR_ELEMENT_NAME(CHECK_BOX),
+	COLOR_ELEMENT_NAME(CHECK_MARK),
+	COLOR_ELEMENT_NAME(CHECK_TEXT),
+	COLOR_ELEMENT_NAME(CLOCK),
+	COLOR_ELEMENT_NAME(HELP_BOLD),
+	COLOR_ELEMENT_NAME(HOSTNAME),
+	COLOR_ELEMENT_NAME(CPU_NICE),
+	COLOR_ELEMENT_NAME(CPU_NICE_TEXT),
+	COLOR_ELEMENT_NAME(CPU_NORMAL),
+	COLOR_ELEMENT_NAME(CPU_KERNEL),
+	COLOR_ELEMENT_NAME(CPU_IOWAIT),
+	COLOR_ELEMENT_NAME(CPU_IRQ),
+	COLOR_ELEMENT_NAME(CPU_SOFTIRQ),
+	COLOR_ELEMENT_NAME(CPU_STEAL),
+	COLOR_ELEMENT_NAME(CPU_GUEST),
+#undef COLOR_ELEMENT_NAME
+};
+
+static int get_color_by_name(const char *name, unsigned int len) {
+	switch(len) {
+		case 3:
+			if(memcmp(name, "RED", 3) == 0) return Red;
+			break;
+		case 4:
+			if(memcmp(name, "BLUE", 4) == 0) return Blue;
+			if(memcmp(name, "CYAN", 4) == 0) return Cyan;
+			break;
+		case 5:
+			if(memcmp(name, "BLACK", 5) == 0) return Black;
+			if(memcmp(name, "GREEN", 5) == 0) return Green;
+			if(memcmp(name, "WHITE", 5) == 0) return White;
+			break;
+		case 6:
+			if(memcmp(name, "YELLOW", 6) == 0) return Yellow;
+			break;
+		case 7:
+			if(memcmp(name, "MAGENTA", 7) == 0) return Magenta;
+			break;
+	}
+	return -1;
+}
+
+static int get_attribute_by_name(const char *name, unsigned int len) {
+#define CHECK_ATTRIBUTE_NAME(A) if(len == sizeof #A - 1 && memcmp(name, #A, sizeof #A - 1) == 0) return A_##A
+	CHECK_ATTRIBUTE_NAME(STANDOUT);
+	CHECK_ATTRIBUTE_NAME(UNDERLINE);
+	CHECK_ATTRIBUTE_NAME(REVERSE);
+	CHECK_ATTRIBUTE_NAME(BLINK);
+	CHECK_ATTRIBUTE_NAME(DIM);
+	CHECK_ATTRIBUTE_NAME(BOLD);
+	CHECK_ATTRIBUTE_NAME(ALTCHARSET);
+	CHECK_ATTRIBUTE_NAME(INVIS);
+	CHECK_ATTRIBUTE_NAME(PROTECT);
+	CHECK_ATTRIBUTE_NAME(HORIZONTAL);
+	CHECK_ATTRIBUTE_NAME(LEFT);
+	CHECK_ATTRIBUTE_NAME(LOW);
+	CHECK_ATTRIBUTE_NAME(RIGHT);
+	CHECK_ATTRIBUTE_NAME(TOP);
+	CHECK_ATTRIBUTE_NAME(VERTICAL);
+	return -1;
+#undef CHECK_ATTRIBUTE_NAME
+}
+
+static int parse_attribute(const char *s) {
+	const char *sep = strchr(s, ',');
+	if(!sep) return -1;
+	int foreground = get_color_by_name(s, sep - s);
+	if(foreground < 0) return -1;
+	s = sep + 1;
+	unsigned int len = 0;
+	while(s[len] && s[len] != '\n' && s[len] != ',') len++;
+	if(!len) return -1;
+	int background = get_color_by_name(s, len);
+	if(background < 0) return -1;
+	int attributes = ColorPair(foreground, background);
+	while(s[len] == ',') {
+		s += len + 1;
+		len = 0;
+		while(s[len] && s[len] != '\n' && s[len] != ',') len++;
+		int attr = get_attribute_by_name(s, len);
+		if(attr < 0) return -1;
+		attributes |= attr;
+	}
+	return attributes;
+}
+
+static void load_user_defined_color_scheme(const char *path) {
+	unsigned int i;
+	FILE *f = fopen(path, "r");
+	if(!f) {
+		perror(path);
+		return;
+	}
+	char *name = NULL;
+	//int *colors = xCalloc(LAST_COLORELEMENT, sizeof(int));
+	int colors[LAST_COLORELEMENT];
+	for(i = 0; i < LAST_COLORELEMENT; i++) colors[i] = A_NORMAL;
+	char buffer[128];
+	unsigned int nlines = 1;
+	while(fgets(buffer, sizeof buffer, f)) {
+		if(strncmp(buffer, "NAME=", 5) == 0) {
+			if(!buffer[5]) {
+				//free(colors);
+				fprintf(stderr, "%s:%u: NAME is empty\n", path, nlines);
+				return;
+			}
+			//name = xStrdup(buffer + 5);
+			size_t len = strlen(buffer + 5);
+			if(buffer[5 + len - 1] == '\n') len--;
+			name = xMalloc(len + 1);
+			memcpy(name, buffer + 5, len);
+			name[len] = 0;
+		} else {
+			int attr = -1;
+			for(i = 0; i < LAST_COLORELEMENT; i++) {
+				const struct key_name *key = color_element_name_map + i;
+				if(strncmp(buffer, key->key_prefix, key->key_length)) continue;
+				attr = parse_attribute(buffer + key->key_length);
+				if(attr < 0) {
+					buffer[key->key_length - 1] = 0;
+					fprintf(stderr, "%s:%u: Warning: failed to parse attribute for %s\n",
+						path, nlines, buffer);
+					attr = 0;
+				} else {
+					colors[i] = attr;
+				}
+				break;
+			}
+			if(attr < 0) {
+				fprintf(stderr, "%s:%u: Warning: unrecognized key\n", path, nlines);
+			}
+		}
+		nlines++;
+	}
+	fclose(f);
+	if(!name) {
+		fprintf(stderr, "%s: NAME is not defined\n", path);
+		return;
+	}
+	CRT_color_scheme_names =
+		xRealloc(CRT_color_scheme_names, (CRT_color_scheme_count + 1) * sizeof(const char *));
+	CRT_color_scheme_names[CRT_color_scheme_count] = name;
+	i = CRT_color_scheme_count++ - LAST_COLORSCHEME;
+	CRT_user_defined_color_schemes =
+		xRealloc(CRT_user_defined_color_schemes, (i + 1) * sizeof colors);
+	memcpy(CRT_user_defined_color_schemes + i, colors, sizeof colors);
+}
+
 void CRT_initColorSchemes() {
    initscr();
 
    assert(CRT_color_scheme_names == NULL);
 
-   CRT_color_scheme_names = xMalloc(8 * sizeof(const char *));
-   CRT_color_scheme_names[0] = "Default",
-   CRT_color_scheme_names[1] = "Monochromatic",
-   CRT_color_scheme_names[2] = "Black on White",
-   CRT_color_scheme_names[3] = "Light Terminal",
-   CRT_color_scheme_names[4] = "MC",
-   CRT_color_scheme_names[5] = "Black Night",
-   CRT_color_scheme_names[6] = "Broken Gray",
-   CRT_color_scheme_names[7] = NULL;
+   CRT_color_scheme_names = xMalloc(LAST_COLORSCHEME * sizeof(const char *));
+   CRT_color_scheme_names[DEFAULT_COLOR_SCHEME] = "Default",
+   CRT_color_scheme_names[MONOCHROME_COLOR_SCHEME] = "Monochromatic",
+   CRT_color_scheme_names[BLACKONWHITE_COLOR_SCHEME] = "Black on White",
+   CRT_color_scheme_names[LIGHTTERMINAL_COLOR_SCHEME] = "Light Terminal",
+   CRT_color_scheme_names[MIDNIGHT_COLOR_SCHEME] = "MC",
+   CRT_color_scheme_names[BLACKNIGHT_COLOR_SCHEME] = "Black Night",
+   CRT_color_scheme_names[BROKENGRAY_COLOR_SCHEME] = "Broken Gray",
 
    assert(CRT_user_defined_color_schemes == NULL);
-   // TODO: Read custom color schemes
+   char *config_dir_path = CRT_getConfigDirPath(NULL);
+   DIR *config_dir = opendir(config_dir_path);
+   if(config_dir) {
+      struct dirent *e;
+      while((e = readdir(config_dir))) {
+         if(*e->d_name == '.') continue;
+         size_t len = strlen(e->d_name);
+         if(len < 13) continue;
+         if(strcmp(e->d_name + (len - 12), ".colorscheme")) continue;
+         char *path = String_cat(config_dir_path, e->d_name);
+         struct stat st;
+         if(stat(path, &st) == 0 && S_ISREG(st.st_mode)) {
+            load_user_defined_color_scheme(path);
+         }
+         free(path);
+      }
+      closedir(config_dir);
+   }
+   free(config_dir_path);
 
    for (int i = 0; i < LAST_COLORELEMENT; i++) {
       unsigned int color = CRT_colorSchemes[DEFAULT_COLOR_SCHEME][i];
