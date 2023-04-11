42de1081aa26eeaab9e99f8273632689570fd73c
WHR <msl0000023508@gmail.com>
2023-04-11 12:22:10 +0800

    rm(1) output file first before writing for safety

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,7 @@
 set -e
 [ -d m4 ] || mkdir m4 || exit
 autoreconf --install --force
+rm -f config.sub.new
 sed -E 's/( |\-)linux\-uclibc\*/\1linux\-uc\*/g' config.sub > config.sub.new
 chmod +x config.sub.new
 mv config.sub.new config.sub
