cfef2f081d1623b7d9bb5270ac81a8cb155c4e7a
WHR <msl0000023508@gmail.com>
2023-10-23 15:44:19 +0800

    Make scripts/MakeHeader.sh available under any version of the GNU GPL as for other files

--- scripts/MakeHeader.sh
+++ scripts/MakeHeader.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-# Copyright 2015-2022 Rivoreo
+# Copyright 2015-2023 Rivoreo
 
 # This program is free software; you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# under the terms of any version of the GNU General Public License as
+# published by the Free Software Foundation.
 
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
